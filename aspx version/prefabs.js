52d6201123478a1ed0ad25532bbfc3fc4e810a8c
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2023-04-17 16:10:39 +0200

    aspx

---
+++ aspx version/prefabs.js
@@ -0,0 +1,11 @@
+
+const category_item_prefab = `
+    <li class="category_item" onclick='window.location="selection.aspx?name=NAME"'> <img src="data/categories/NAME/icon.png"> <a> NAME  </a> </li>
+`
+
+
+const selection_item_prefab = `
+    <li class="category_item" onclick='window.location="template.aspx?category=KAT&name=NAME"'> <a> 
+        <img src="data/categories/KAT/NAME">
+    </a> </li>
+`
